7e03d0963332ba921b4ac0510d64227ac83365f7
Baketoo1 <martinek.svrcek@gmail.com>
2023-06-26 22:09:57 +0200

    opravení chyb a přídání Advanced-3

--- Advanced-3/script.js
+++ Advanced-3/script.js
@@ -1,1 +1,14 @@
+// zadaní variables.
+const input = document.querySelector(".inputSearch");
+const search = document.querySelector(".search");
+const button = document.querySelector(".btn");
+    // funkce která přidá třidu active.
+button.addEventListener("click",function(){
+    // toggle = přepínač
+    search.classList.toggle("active");
+    // funkce kterou můžeme rovnou psát do pole.
+    input.focus();
+});
+
+
 
